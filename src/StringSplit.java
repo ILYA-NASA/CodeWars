f93e75ffeeff290fc0dbd8f4d80579f6416164b0
ILYA.NASA <il.nasa@outlook.com>
2022-11-04 11:30:55 +0300

    added my solution

---
+++ src/StringSplit.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class StringSplit {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution("abc")));
+    }
+
+    public static String[] solution(String s) {
+        //Write your code here
+        String[] res = new String[((s.length() + 1) / 2)];
+        if (s.length() % 2 == 0) {
+            for (int i = 0; i < res.length; i++) {
+                res[i] = s.substring(i * 2, i * 2 + 2);
+            }
+        } else {
+            for (int i = 0; i < res.length - 1; i++) {
+                res[i] = s.substring(i * 2, i * 2 + 2);
+            }
+            res[res.length - 1] = s.substring(s.length() - 1) + "_";
+        }
+        return res;
+    }
+}

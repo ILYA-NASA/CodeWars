ce19ac4a9d3fcb43d79acf6493278eb2998c485e
ILYA.NASA <il.nasa@outlook.com>
2022-11-04 11:35:14 +0300

    added short solution using regex (in comment)

--- src/StringSplit.java
+++ src/StringSplit.java
@@ -21,3 +21,10 @@ public class StringSplit {
         return res;
     }
 }
+
+//public class StringSplit {
+//    public static String[] solution(String s) {
+//        s = (s.length() % 2 == 0)?s:s+"_";
+//        return s.split("(?<=\\G.{2})");
+//    }
+//}

4c8e3159da3067c616a504d3e5c28626f3256ef3
ILYA.NASA <il.nasa@outlook.com>
2022-11-04 11:44:49 +0300

    correct syntax

--- src/StringSplit.java
+++ src/StringSplit.java
@@ -5,26 +5,24 @@ public class StringSplit {
         System.out.println(Arrays.toString(solution("abc")));
     }
 
+//    public static String[] solution(String s) {
+//        //Write your code here
+//        String[] res = new String[((s.length() + 1) / 2)];
+//        if (s.length() % 2 == 0) {
+//            for (int i = 0; i < res.length; i++) {
+//                res[i] = s.substring(i * 2, i * 2 + 2);
+//            }
+//        } else {
+//            for (int i = 0; i < res.length - 1; i++) {
+//                res[i] = s.substring(i * 2, i * 2 + 2);
+//            }
+//            res[res.length - 1] = s.substring(s.length() - 1) + "_";
+//        }
+//        return res;
+//    }
+
     public static String[] solution(String s) {
-        //Write your code here
-        String[] res = new String[((s.length() + 1) / 2)];
-        if (s.length() % 2 == 0) {
-            for (int i = 0; i < res.length; i++) {
-                res[i] = s.substring(i * 2, i * 2 + 2);
-            }
-        } else {
-            for (int i = 0; i < res.length - 1; i++) {
-                res[i] = s.substring(i * 2, i * 2 + 2);
-            }
-            res[res.length - 1] = s.substring(s.length() - 1) + "_";
-        }
-        return res;
+        s = (s.length() % 2 == 0) ? s : s + "_";
+        return s.split("(?<=\\G.{2})");
     }
 }
-
-//public class StringSplit {
-//    public static String[] solution(String s) {
-//        s = (s.length() % 2 == 0)?s:s+"_";
-//        return s.split("(?<=\\G.{2})");
-//    }
-//}
